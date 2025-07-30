fcaea266a47181d9914b8fe9b939b32fbf564c7c
tasnim <tasnimghanmi02@gmail.com>
2025-07-30 14:59:49 +0100

    XG Boots & random forest

---
+++ ML/supervisedLearning/classification/random-forests-and-XGBoots.py
@@ -0,0 +1,47 @@
+#predicting house prices
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, r2_score
+import time
+
+# Load the California Housing dataset
+data = fetch_california_housing()
+X, y = data.data, data.target
+
+# Split data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize models
+n_estimators=100
+rf = RandomForestRegressor(n_estimators=n_estimators, random_state=42)
+xgb = XGBRegressor(n_estimators=n_estimators, random_state=42)
+
+# Fit models
+# Measure training time for Random Forest
+start_time_rf = time.time()
+rf.fit(X_train, y_train)
+end_time_rf = time.time()
+rf_train_time = end_time_rf - start_time_rf
+
+# Measure training time for XGBoost
+start_time_xgb = time.time()
+xgb.fit(X_train, y_train)
+end_time_xgb = time.time()
+xgb_train_time = end_time_xgb - start_time_xgb
+
+# Measure prediction time for Random Forest
+start_time_rf = time.time()
+y_pred_rf =rf.predict(X_test) 
+end_time_rf = time.time()
+rf_pred_time = end_time_rf-start_time_rf
+
+# Measure prediction time for XGBoost
+start_time_xgb = time.time()
+y_pred_xgb = xgb.predict(X_test)
+end_time_xgb =  time.time()
+xgb_pred_time = end_time_xgb-start_time_xgb
