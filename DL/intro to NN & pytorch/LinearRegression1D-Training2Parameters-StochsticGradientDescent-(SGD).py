c7d3cbc98711ebe4e03f2d206027134c50baba77
tasnim <tasnimghanmi02@gmail.com>
2025-08-05 09:47:29 +0100

    SGD

---
+++ DL/intro to NN & pytorch/LinearRegression1D-Training2Parameters-StochsticGradientDescent-(SGD).py
@@ -0,0 +1,203 @@
+import torch
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits import mplot3d
+from torch.utils.data import Dataset, DataLoader
+
+
+# The class for plot the diagram
+class plot_error_surfaces(object):
+    
+    # Constructor
+    def __init__(self, w_range, b_range, X, Y, n_samples = 30, go = True):
+        W = np.linspace(-w_range, w_range, n_samples)
+        B = np.linspace(-b_range, b_range, n_samples)
+        w, b = np.meshgrid(W, B)    
+        Z = np.zeros((30, 30))
+        count1 = 0
+        self.y = Y.numpy()
+        self.x = X.numpy()
+        for w1, b1 in zip(w, b):
+            count2 = 0
+            for w2, b2 in zip(w1, b1):
+                Z[count1, count2] = np.mean((self.y - w2 * self.x + b2) ** 2)
+                count2 += 1
+            count1 += 1
+        self.Z = Z
+        self.w = w
+        self.b = b
+        self.W = []
+        self.B = []
+        self.LOSS = []
+        self.n = 0
+        if go == True:
+            plt.figure()
+            plt.figure(figsize = (7.5, 5))
+            plt.axes(projection = '3d').plot_surface(self.w, self.b, self.Z, rstride = 1, cstride = 1,cmap = 'viridis', edgecolor = 'none')
+            plt.title('Loss Surface')
+            plt.xlabel('w')
+            plt.ylabel('b')
+            plt.show()
+            plt.figure()
+            plt.title('Loss Surface Contour')
+            plt.xlabel('w')
+            plt.ylabel('b')
+            plt.contour(self.w, self.b, self.Z)
+            plt.show()
+    
+    # Setter
+    def set_para_loss(self, W, B, loss):
+        self.n = self.n + 1
+        self.W.append(W)
+        self.B.append(B)
+        self.LOSS.append(loss)
+    
+    # Plot diagram
+    def final_plot(self): 
+        ax = plt.axes(projection = '3d')
+        ax.plot_wireframe(self.w, self.b, self.Z)
+        ax.scatter(self.W, self.B, self.LOSS, c = 'r', marker = 'x', s = 200, alpha = 1)
+        plt.figure()
+        plt.contour(self.w, self.b, self.Z)
+        plt.scatter(self.W, self.B, c = 'r', marker = 'x')
+        plt.xlabel('w')
+        plt.ylabel('b')
+        plt.show()
+    
+    # Plot diagram
+    def plot_ps(self):
+        plt.subplot(121)
+        plt.ylim
+        plt.plot(self.x, self.y, 'ro', label = "training points")
+        plt.plot(self.x, self.W[-1] * self.x + self.B[-1], label = "estimated line")
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.ylim((-10, 15))
+        plt.title('Data Space Iteration: ' + str(self.n))
+        plt.subplot(122)
+        plt.contour(self.w, self.b, self.Z)
+        plt.scatter(self.W, self.B, c = 'r', marker = 'x')
+        plt.title('Loss Surface Contour Iteration' + str(self.n))
+        plt.xlabel('w')
+        plt.ylabel('b')
+        plt.show()
+
+
+torch.manual_seed(1)
+X=torch.arrange(-3,3,0.1).view(-1,1)
+f=X-1
+Y=f+0.1*torch.randn(X.size())
+
+w = torch.tensor(-15.0, requires_grad = True)
+b = torch.tensor(-10.0, requires_grad = True)
+
+lr = 0.1
+LOSS_SGD = []
+
+get_surface = plot_error_surfaces(15, 13, X, Y, 30)
+
+
+#creating model
+def forward(x):
+    return w*x
+
+def criterion(yhat,y):
+    return torch.mean((yhat-y)**2)
+
+#training
+def train_model_SGD(iter):
+    for epochs in range(iter):
+        #predict
+        yhat=forward(X)
+
+        #store SGD
+        LOSS_SGD.append(criterion(yhat,Y).tolist())
+        
+        for x,y in zip(X,Y):
+            yhat=forward(x)
+            loss=criterion(yhat,y)
+
+            
+            # backward pass: compute gradient of the loss with respect to all the learnable parameters
+            loss.backward()
+            
+            #update prs
+            w.data=w.data-lr*w.grad.data
+            b.data=b.data-lr*b.grad.data
+
+            w.grad.data.zero_()
+            b.grad.data.zero_()
+            
+
+train_model_SGD(10)
+# Plot out LOSS_SGD
+plt.plot(LOSS_SGD,label = "Stochastic Gradient Descent")
+plt.xlabel('epoch')
+plt.ylabel('Cost/ total loss')
+plt.legend()
+plt.show()
+
+
+
+#dataloader
+class Data(Dataset):
+    
+    # Constructor
+    def __init__(self):
+        self.x = torch.arange(-3, 3, 0.1).view(-1, 1)
+        self.y = 1 * self.x - 1
+        self.len = self.x.shape[0]
+        
+    # Getter
+    def __getitem__(self,index):    
+        return self.x[index], self.y[index]
+    
+    # Return the length
+    def __len__(self):
+        return self.len
+
+
+dataset = Data()
+trainloader = DataLoader(dataset = dataset, batch_size = 1)
+
+w = torch.tensor(-15.0,requires_grad=True)
+b = torch.tensor(-10.0,requires_grad=True)
+LOSS_Loader = []
+
+def train_model_DataLoader(epochs):
+    
+    # Loop
+    for epoch in range(epochs):
+        
+        # SGD is an approximation of out true total loss/cost, in this line of code we calculate our true loss/cost and store it
+        Yhat = forward(X)
+        
+        # store the loss 
+        LOSS_Loader.append(criterion(Yhat, Y).tolist())
+        
+        for x, y in trainloader:
+            
+            # make a prediction
+            yhat = forward(x)
+            
+            # calculate the loss
+            loss = criterion(yhat, y)
+            
+            # Section for plotting
+            get_surface.set_para_loss(w.data.tolist(), b.data.tolist(), loss.tolist())
+            
+            # Backward pass: compute gradient of the loss with respect to all the learnable parameters
+            loss.backward()
+            
+            # Updata parameters slope
+            w.data = w.data - lr * w.grad.data
+            b.data = b.data - lr* b.grad.data
+            
+            # Clear gradients 
+            w.grad.data.zero_()
+            b.grad.data.zero_()
+            
+        #plot surface and data space after each epoch    
+        get_surface.plot_ps()
+
+train_model_DataLoader(10)
