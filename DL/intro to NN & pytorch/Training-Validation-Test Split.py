95efa2afa8418bd0f5279fae13c58f113489b996
tasnim <tasnimghanmi02@gmail.com>
2025-08-06 20:07:28 +0100

    training validation & splitting

---
+++ DL/intro to NN & pytorch/Training-Validation-Test Split.py
@@ -0,0 +1,80 @@
+from torch.utils.data import Dataset, DataLoader
+from torch import nn
+import torch
+
+class Data(Dataset):
+    
+    # Constructor
+    def __init__(self, train = True):
+            self.x = torch.arange(-3, 3, 0.1).view(-1, 1)
+            self.f = -3 * self.x + 1
+            self.y = self.f + 0.1 * torch.randn(self.x.size())
+            self.len = self.x.shape[0]
+            
+            #outliers 
+            if train == True:
+                self.y[0] = 0
+                self.y[50:55] = 20
+            else:
+                pass
+      
+    # Getter
+    def __getitem__(self, index):    
+        return self.x[index], self.y[index]
+    
+    # Get Length
+    def __len__(self):
+        return self.len
+    
+train_data = Data()
+val_data = Data(train = False)
+
+
+class linear_regression(nn.Module):
+    
+    # Constructor
+    def __init__(self, input_size, output_size):
+        super(linear_regression, self).__init__()
+        self.linear = nn.Linear(input_size, output_size)
+    
+    # Prediction function
+    def forward(self, x):
+        yhat = self.linear(x)
+        return yhat
+    
+criterion = nn.MSELoss()
+trainloader = DataLoader(dataset = train_data, batch_size = 1)
+
+learning_rates=[0.0001, 0.001, 0.01, 0.1]
+
+train_error=torch.zeros(len(learning_rates))
+validation_error=torch.zeros(len(learning_rates))
+
+MODELS=[]
+
+def train_model_with_lr (iter, lr_list):
+    
+    # iterate through different learning rates 
+    for i, lr in enumerate(lr_list):
+        model = linear_regression(1, 1)
+        optimizer = optim.SGD(model.parameters(), lr = lr)
+        for epoch in range(iter):
+            for x, y in trainloader:
+                yhat = model(x)
+                loss = criterion(yhat, y)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                
+        # train data
+        Yhat = model(train_data.x)
+        train_loss = criterion(Yhat, train_data.y)
+        train_error[i] = train_loss.item()
+    
+        # validation data
+        Yhat = model(val_data.x)
+        val_loss = criterion(Yhat, val_data.y)
+        validation_error[i] = val_loss.item()
+        MODELS.append(model)
+
+train_model_with_lr(10, learning_rates)
