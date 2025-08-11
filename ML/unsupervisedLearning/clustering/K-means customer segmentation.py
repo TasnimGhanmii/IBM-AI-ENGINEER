1d98cf4badf6d186e18f5500993a1e060e8d4c1a
tasnim <tasnimghanmi02@gmail.com>
2025-08-11 10:03:20 +0100

    K-mean

---
+++ ML/unsupervisedLearning/clustering/K-means customer segmentation.py
@@ -0,0 +1,42 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans 
+from sklearn.datasets import make_blobs 
+from sklearn.preprocessing import StandardScaler
+import plotly.express as px
+
+#preprocessing
+cust_df = pd.read_csv("https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%204/data/Cust_Segmentation.csv")
+cust_df = cust_df.drop('Address', axis=1)
+# Drop NaNs from the dataframe
+cust_df = cust_df.dropna()
+cust_df.info()
+X = cust_df.values[:,1:] # leaves out `Customer ID`
+Clus_dataSet = StandardScaler().fit_transform(X)
+
+#k-mean
+clusterNum = 3
+k_means = KMeans(init = "k-means++", n_clusters = clusterNum, n_init = 12)
+k_means.fit(X)
+labels = k_means.labels_
+cust_df["Clus_km"] = labels
+cust_df.groupby('Clus_km').mean()
+
+area = np.pi * ( X[:, 1])**2  
+plt.scatter(X[:, 0], X[:, 3], s=area, c=labels.astype(float), cmap='tab10', ec='k',alpha=0.5)
+plt.xlabel('Age', fontsize=18)
+plt.ylabel('Income', fontsize=16)
+plt.show()
+
+# Create interactive 3D scatter plot
+fig = px.scatter_3d(X, x=1, y=0, z=3, opacity=0.7, color=labels.astype(float))
+
+fig.update_traces(marker=dict(size=5, line=dict(width=.25)), showlegend=False)
+fig.update_layout(coloraxis_showscale=False, width=1000, height=800, scene=dict(
+        xaxis=dict(title='Education'),
+        yaxis=dict(title='Age'),
+        zaxis=dict(title='Income')
+    ))  # Remove color bar, resize plot
+
+fig.show()
