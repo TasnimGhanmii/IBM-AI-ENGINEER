1153fc67e497e05409d199dce5d3fbfd88851e5c
tasnim <tasnimghanmi02@gmail.com>
2025-08-14 17:16:10 +0100

    softmax 2D

--- DL/DL with pytorch/Softmax Classifier2D.py
+++ DL/DL with pytorch/Softmax Classifier2D.py
@@ -16,4 +16,41 @@ class SoftMax(nn.Module):
     def forward(self,x):
         z=self.linear(x)
         return z
+    
+#model
+input_dim = 28 * 28
+output_dim = 10
+model = SoftMax(input_dim, output_dim)
 
+learning_rate = 0.1
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss()
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100)
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=5000)
+
+#train
+n_epochs = 10
+loss_list = []
+accuracy_list = []
+N_test = len(validation_dataset)
+
+def train_model(n_epochs):
+    for epoch in range(n_epochs):
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            z = model(x.view(-1, 28 * 28))
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            
+        correct = 0
+        # perform a prediction on the validationdata  
+        for x_test, y_test in validation_loader:
+            z = model(x_test.view(-1, 28 * 28))
+            _, yhat = torch.max(z.data, 1)
+            correct += (yhat == y_test).sum().item()
+        accuracy = correct / N_test
+        loss_list.append(loss.data)
+        accuracy_list.append(accuracy)
+
+train_model(n_epochs)
