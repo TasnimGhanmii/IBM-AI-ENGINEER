4367d11da8beeb1f9f6b8b14721ce65599305a12
tasnim <tasnimghanmi02@gmail.com>
2025-08-15 15:39:13 +0100

    hidden neurons

---
+++ DL/DL with pytorch/Hidden Neurons.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader,Dataset
+
+class Data(Dataset):
+    def __init__(self):
+        self.x=torch.linspace(-20, 20, 100).view(-1,1)
+  
+        self.y=torch.zeros(self.x.shape[0])
+        self.y[(self.x[:,0]>-10)& (self.x[:,0]<-5)]=1
+        self.y[(self.x[:,0]>5)& (self.x[:,0]<10)]=1
+        self.y=self.y.view(-1,1)
+        self.len=self.x.shape[0]
+
+    def __getitem__(self, index):
+        return self.x[index],self.y[index]
+    
+    def __len__(self):
+        return self.len
+    
+class NN(nn.Module):
+    def __init__(self, D_in,H,D_out):
+        super(NN,self).__init__()
+        self.linear1=nn.Linear(D_in,H)
+        self.linear2=nn.Linear(H,D_out)
+
+    def forward(self,x):
+        x=torch.sigmoid(self.linear1(x))  
+        x=torch.sigmoid(self.linear2(x))
+        return x
+    
+
+def train(dataset,model,criterion, train_loader, optimizer,epochs=5,):
+    cost=[]
+    for epoch in range(epochs):
+        total=0
+        
+        for x,y in train_loader:
+            optimizer.zero_grad()
+            
+            yhat=model(x)
+            loss=criterion(yhat,y)
+            loss.backward()
+            optimizer.step()
+            total+=loss.item() 
+        cost.append(total)
+    return cost
+
+dataset=Data()
+torch.manual_seed(0)
+model=NN(1,9,1)
+learning_rate=0.1
+criterion=nn.BCELoss()
+optimizer=torch.optim.Adam(model.parameters(), lr=learning_rate)
+train_loader=DataLoader(dataset=dataset,batch_size=100)
+COST=train(dataset,model,criterion, train_loader, optimizer, epochs=600,plot_number=200)
