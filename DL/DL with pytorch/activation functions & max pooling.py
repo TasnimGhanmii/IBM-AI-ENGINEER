46f0f74fc88a7906f04500df22205f56385e0973
tasnim <tasnimghanmi02@gmail.com>
2025-08-22 12:58:15 +0100

    comment

--- DL/DL with pytorch/activation functions & max pooling.py
+++ DL/DL with pytorch/activation functions & max pooling.py
@@ -16,8 +16,9 @@ image1[0,0,0,:]=torch.tensor([1.0,2.0,3.0,-4.0])
 image1[0,0,1,:]=torch.tensor([0.0,2.0,-3.0,0.0])
 image1[0,0,2,:]=torch.tensor([0.0,2.0,3.0,1.0])
 
+#The region shifts, and the process is repeated. The process is similar to convolution and is demonstrated in the following figure:
 max1=torch.nn.MaxPool2d(2,stride=1)
 max1(image1)
-
+#If the stride is set to None (its defaults setting), the process will simply take the maximum in a prescribed area and shift over block by block:
 max1=torch.nn.MaxPool2d(2)
 max1(image1)
