e92ad3bb9852aa9b3e6311cf19db95a08a08ffd2
tasnim <tasnimghanmi02@gmail.com>
2025-08-23 11:41:20 +0100

    cnn

---
+++ DL/DL with pytorch/Convolutional Neural Network MNIST.py
@@ -0,0 +1,86 @@
+import torch 
+import torch.nn as nn
+import torchvision.transforms as transforms
+import torchvision.datasets as dsets
+import matplotlib.pylab as plt
+import numpy as np
+
+IMAGE_SIZE = 16
+composed = transforms.Compose([transforms.Resize((IMAGE_SIZE, IMAGE_SIZE)), transforms.ToTensor()])
+train_dataset = dsets.MNIST(root='./data', train=True, download=True, transform=composed)
+validation_dataset = dsets.MNIST(root='./data', train=False, download=True, transform=composed)
+
+class CNN(nn.Module):
+    
+    # Contructor
+    def __init__(self, out_1=16, out_2=32):
+        super(CNN, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels=1, out_channels=out_1, kernel_size=5, padding=2)
+        self.maxpool1=nn.MaxPool2d(kernel_size=2)
+
+        self.cnn2 = nn.Conv2d(in_channels=out_1, out_channels=out_2, kernel_size=5, stride=1, padding=2)
+        self.maxpool2=nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(out_2 * 4 * 4, 10)
+    
+    # Prediction
+    def forward(self, x):
+        x = self.cnn1(x)
+        x = torch.relu(x)
+        x = self.maxpool1(x)
+        x = self.cnn2(x)
+        x = torch.relu(x)
+        x = self.maxpool2(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        return x
+    
+    # Outputs in each steps
+    def activations(self, x):
+        #outputs activation this is not necessary
+        z1 = self.cnn1(x)
+        a1 = torch.relu(z1)
+        out = self.maxpool1(a1)
+        
+        z2 = self.cnn2(out)
+        a2 = torch.relu(z2)
+        out1 = self.maxpool2(a2)
+        out = out.view(out.size(0),-1)
+        return z1, a1, z2, a2, out1,out
+    
+model = CNN(out_1=16, out_2=32)
+criterion = nn.CrossEntropyLoss()
+learning_rate = 0.1
+optimizer = torch.optim.SGD(model.parameters(), lr = learning_rate)
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=100)
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=5000)
+
+# Train the model
+
+n_epochs=3
+cost_list=[]
+accuracy_list=[]
+N_test=len(validation_dataset)
+COST=0
+
+def train_model(n_epochs):
+    for epoch in range(n_epochs):
+        COST=0
+        for x, y in train_loader:
+            optimizer.zero_grad()
+            z = model(x)
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            COST+=loss.data
+        
+        cost_list.append(COST)
+        correct=0
+        #perform a prediction on the validation  data  
+        for x_test, y_test in validation_loader:
+            z = model(x_test)
+            _, yhat = torch.max(z.data, 1)
+            correct += (yhat == y_test).sum().item()
+        accuracy = correct / N_test
+        accuracy_list.append(accuracy)
+     
+train_model(n_epochs)
